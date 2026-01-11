f1c43b8f8e18e7e279b74dcbd33f8b7f2a55666d
mohamadreza <mohamadrezadoustii84@gmial.com>
2026-01-11 18:43:57 +0330

    operator_temp.py edited

--- temperature/operator_temp.py
+++ temperature/operator_temp.py
@@ -1,38 +1,39 @@
 class Change():
     def __init__(self, f, t, amount):
-        self.f = f
-        self.t = t
-        self.amount = amount
+        self.From = f
+        self.To = t
+        self.entry = amount
     
-    def change(self, From, To, entry):
-        if From=='Celsius' and To=='Celsius':
-            return entry
-        elif From=='Celsius' and To=='Kelvin':
-            entry += 273.15
-            return entry
-        elif From=='Celsius' and To=='Fahrenheit':
-            entry *= 1.8
-            entry += 32
-            return entry
-        elif From=='Kelvin' and To=='Celsius':
-            entry -= 273.15
-            return entry
-        elif From=='Kelvin' and To=='Kelvin':
-            return entry
-        elif From=='Kelvin' and To=='Fahrenheit':
-            res = 1.8*(entry-273.15)+32 
-            return res
-        elif From=='Fahrenheit' and To=='Fahrenheit':
-            return entry
-        elif From=='Fahrenheit' and To=='Kelvin':
-            res = entry -32
-            res *= 5
-            res /= 9
-            res += 273.15
-            return res
+    def change(self):
+        if self.From=='Celsius' and self.To=='Celsius':
+            return self.entry
+        elif self.From=='Celsius' and self.To=='Kelvin':
+            self.entry += 273.15
+            return self.entry
+        elif self.From=='Celsius' and self.To=='Fahrenheit':
+            self.entry *= 1.8
+            self.entry += 32
+            return self.entry
+        elif self.From=='Kelvin' and self.To=='Celsius':
+            self.entry -= 273.15
+            return self.entry
+        elif self.From=='Kelvin' and self.To=='Kelvin':
+            return self.entry
+        elif self.From=='Kelvin' and self.To=='Fahrenheit':
+            self.res = 1.8*(self.entry-273.15)+32 
+            return self.res
+        elif self.From=='Fahrenheit' and self.To=='Fahrenheit':
+            return self.entry
+        elif self.From=='Fahrenheit' and self.To=='Kelvin':
+            self.res = self.entry -32
+            self.res *= 5
+            self.res /= 9
+            self.res += 273.15
+            return self.res
+        elif self.From=='Fahrenheit' and self.To=='Celsius':
+            self.res = self.entry -32
+            self.res *= 5
+            self.res /= 9
+            return self.res
         else:
-            res = entry -32
-            res *= 5
-            res /= 9
-            return res
-        
+            return "please fill the box"
