5a6a0fe817c8b80d8a36288ec18265e124828540
mohamadreza <mohamadrezadoustii84@gmial.com>
2026-01-16 22:43:50 +0330

    fix entry errors in app_temperature.py

--- temperature/app_temperature.py
+++ temperature/app_temperature.py
@@ -1,17 +1,24 @@
 from temperature.operator_temp import Change
+import re
 
-class AppTemperature():
+class AppTemperature(): 
     def __init__(self):
         self.f = 0
         self.t = 0
         self.entry = 0
 
     def apply(self, aply, result):
-        if not aply.get() == '':
-            self.entry = float(aply.get())
-        self.operator = Change(self.f, self.t, self.entry)
-        ansewr = self.operator.change()
-        result.configure(text=ansewr, text_color='black')
+        self.text = aply.get()
+        if re.fullmatch(r"\d*\.\d*", self.text) or re.fullmatch(r"\d+", self.text):
+            if self.text == '.':
+                self.entry = 0.0
+            else:
+                self.entry = float(self.text)
+            self.operator = Change(self.f, self.t, self.entry)
+            ansewr = self.operator.change()
+            result.configure(text=ansewr, text_color='black')
+        else:
+            result.configure(text='enter valid number')
 
     def get_check_from(self, value):
         self.f = value
